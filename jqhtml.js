39537347ef68be541b2452664749cecf9b2ccc6e
supriyasuru <supriya@securitytroops.com>
2020-10-06 11:10:10 +0530

    remove data after refreshing the page

--- jqhtml.js
+++ jqhtml.js
@@ -1,49 +1,61 @@
 $(document).ready(function(){
-	var stor = [];
+	//var stor = [];
   	var data = JSON.parse(localStorage.getItem('stor'));
   	$(data).each(function(hkey,hvalue){
   		$("main").append('<section><h1>'+hvalue.title+'<button onclick=myFunction(this)>X</button></h1></section>');	
   		$(hvalue.sub).each(function(skey,svalue){
 			$( "main section:nth-child(" +(hkey+1)+ ")" ).append( '<div><h2> '+svalue.title+'<button onclick=myFunctionn(this)>X</button></h2></div>' );
-			$(svalue.form).each(function(fkey,fvalue){
+			$(svalue.formm).each(function(fkey,fvalue){
+				console.log(fvalue.option)
 	  			if (fvalue.input == "radio"){
 	  				var rdo=$('<p>')
-	  				$(fvalue.option).each(function(rdkey,rdvalue){
+	  				var optt=fvalue.option.split(",");
+	  				$(optt).each(function(rdkey,rdvalue){
 	  					$('<label>'+rdvalue+'</label><input type="'+fvalue.input+'" id="'+fvalue.id+'" name="'+fvalue.name+'" value="'+fvalue.value+'" placeholder="'+fvalue.placeholder+'" option="'+fvalue.option+'">').appendTo(rdo)
 						rdo.appendTo('main section:nth-child('+(hkey+1)+' ) div:nth-child(' +(skey+2)+ ') ')
 	  				});
+	  				$('<button onclick=myFunform(this)>X</button>').appendTo(rdo)
 	  			}
 	  	  		else if(fvalue.input == "checkbox"){
 	  	  			var chkk=$('<p>')
-
-				 	$(fvalue.option).each(function(chkkey,chkvalue){
+	  	  			var optt=fvalue.option.split(",");
+				 	$(optt).each(function(chkkey,chkvalue){
 				 		$('<label>'+chkvalue+'</label><input type="'+fvalue.input+'" id="'+fvalue.id+'" name="'+fvalue.name+'" value="'+fvalue.value+'" placeholder="'+fvalue.placeholder+'" option="'+fvalue.option+'">').appendTo(chkk)
 						chkk.appendTo('main section:nth-child('+(hkey+1)+' ) div:nth-child(' +(skey+2)+ ') ')
-	  	 			});	  			
+	  	 			});
+	  	 			$('<button onclick=myFunform(this)>X</button>').appendTo(chkk)	  			
 				}
 	  	 		else if (fvalue.input == "textarea"){
-					$( "main section:nth-child(" +(hkey+1)+ ") div:nth-child("+(skey+2)+")" ).append( '<p><label>'+fvalue.label+'</label> <'+fvalue.input+' id="'+fvalue.id+'" name="'+fvalue.name+'"  placeholder="'+fvalue.placeholder+'"></'+fvalue.input+'></p>');
+	  	 			var txt_area=$('<p>')
+					$('<label>'+fvalue.label+'</label><'+fvalue.input+' id="'+fvalue.id+'" name="'+fvalue.name+'" value="'+fvalue.value+'" placeholder="'+fvalue.placeholder+'"></textarea>').appendTo(txt_area)
+					txt_area.appendTo('main section:nth-child('+hkey+1+' ) div:nth-child(' +skey+2+ ') ')
+					$('<button onclick=myFunform(this)>X</button>').appendTo(txt_area)
 	  			}
 	  			else if (fvalue.input == "select"){
 	  				var radioo_select=$(".op").val().split(",");
 	  				var selec=$('<p>')
 					var select_tag=$('<select>').appendTo(selec)
 					var op_select_len =radioo_select.length;
-					$(fvalue.option).each(function(selkkey,selvalue){
+					var optt=fvalue.option.split(",");
+					console.log(optt)
+					$(optt).each(function(selkkey,selvalue){
+						console.log(selvalue)
 						$('<option value="'+selkkey+'">'+selvalue+'</option>').appendTo(select_tag);	
 					});
+					$('<button onclick=myFunform(this)>X</button>').appendTo(selec)
 					selec.appendTo('main section:nth-child('+(hkey+1)+' ) div:nth-child(' +(skey+2)+ ') ')
 				}
 	  			else{
-	  				$( "main section:nth-child(" +(hkey+1)+ ") div:nth-child("+(skey+2)+")" ).append( '<p><label>'+fvalue.label+'</label> <input type="'+fvalue.input+'" id="'+fvalue.id+'" name="'+fvalue.name+'"  placeholder="'+fvalue.placeholder+'"></p>');
+	  				$( "main section:nth-child(" +(hkey+1)+ ") div:nth-child("+(skey+2)+")" ).append( '<p><label>'+fvalue.label+'</label> <input type="'+fvalue.input+'" id="'+fvalue.id+'" name="'+fvalue.name+'"  placeholder="'+fvalue.placeholder+'"><button onclick=myFunform(this)>X</button></p>');
 	  			}
   			});
   		});  		
   	});
 
   	$(".form_head").submit(function(event){
-		event.preventDefault()
+		event.preventDefault()					//using this to hold page
 		var valu=$('.inputhead_class').val();
+		console.log(valu)
 		$("main").append('<section><h1>'+valu+'<button onclick=myFunction(this)>X</button></h1></section>');
 		$(".head_dropdown1 option").remove();
 		$(".formhead_list option").remove();      
@@ -56,10 +68,11 @@ $(document).ready(function(){
 	  		$(".form-group1 select").append('<option value="'+key+'">'+ head +'</option>');	
 		});
 		
-		stor.push({'title':valu,'sub':[]});
-		localStorage.setItem('stor',JSON.stringify(stor));
+		//stor.push({'title':valu,'sub':[]});
+		//localStorage.setItem('stor',JSON.stringify(stor));
 
-		$(".form_head").trigger('reset');	
+		$(".form_head").trigger('reset');
+		strgeform();
   	});
 	
   	$(".formsub_head").submit(function(event){
@@ -70,11 +83,11 @@ $(document).ready(function(){
 		$( "main section:nth-child(" +sel+ ")" ).append( '<div><h2> '+invl+'<button onclick=myFunctionn(this)>X</button></h2></div>' );
 		$(".formsubhead_list option").remove();
 
-		stor[sel-1].sub.push({'title':invl,'form':[] });
-		localStorage.setItem('stor',JSON.stringify(stor));
+		//stor[sel-1].sub.push({'title':invl,'form':[] });
+		//localStorage.setItem('stor',JSON.stringify(stor));
 		
 		$(".formsub_head").trigger('reset');
-
+		strgeform();
   	});
   
   	$( ".formhead_list" ).change(function () {
@@ -88,7 +101,9 @@ $(document).ready(function(){
 		});  
 	}); 
 
-	$("#btn3").click(function(){ 
+	//$("#btn3").click(function(){ 
+	$('.formhead_subhead').submit(function(event){
+		event.preventDefault()
 		var head_select=  $('.formhead_list option:selected').val();
 		var subhead_select=  $('.formsubhead_list option:selected').val();
 		var itype_select=  $('.inputtype_list option:selected').val();
@@ -103,34 +118,28 @@ $(document).ready(function(){
 		var radioo_select=option_value.split(",");
 		var disable_check=$(".check_class").val();
 		var read_only=$(".r_o").val();
-		var disb=false;
-		var read_ly=false;
-		var reqq=false;
-		
+		console.log(itype_select)
 		if (itype_select == "radio"){
-			var rad=$('<p>')
+			var rad=$('<p data-type="'+itype_select+'" data-placeholder="'+pholder_value+'" id-holder="'+inputid_value+'" name-holder="'+name_value+'" op-holder="'+radioo_select+'" lab-holder="'+label_value+'" value-holder="'+value_value+'">')
 			$( radioo_select ).each(function(i,j) {
 				$('<label>'+j+'</label><input type="'+itype_select+'" id="'+inputid_value+'" name="'+name_value+'" value="'+value_value+'" placeholder="'+pholder_value+'" >').appendTo(rad)
 				rad.appendTo('main section:nth-child('+head_select+' ) div:nth-child(' +cc+ ') ')
 				});
 			$('<button onclick=myFunform(this)>X</button>').appendTo(rad)
-			stor[head_select-1].sub[subhead_select-1].form.push({'input':'radio', 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
+			//stor[head_select-1].sub[subhead_select-1].form.push({'input':itype_select, 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
 		}
-		
-
-			
-			
+					
 		else if (itype_select == "checkbox"){
-			var check=$('<p>')
+			var check=$('<p data-type="'+itype_select+'" data-placeholder="'+pholder_value+'" id-holder="'+inputid_value+'" name-holder="'+name_value+'" op-holder="'+radioo_select+'" lab-holder="'+label_value+'">')
 			$( radioo_select ).each(function(i,j) {
 				$('<label>'+j+'</label><input type="'+itype_select+'" id="'+inputid_value+'" name="'+name_value+'" value="'+value_value+'" placeholder="'+pholder_value+'"  >').appendTo(check)
 				check.appendTo('main section:nth-child('+head_select+' ) div:nth-child(' +cc+ ') ')
 			});
-		$('<button onclick=myFunform(this)>X</button>').appendTo(check)
-		stor[head_select-1].sub[subhead_select-1].form.push({'input':'checkbox', 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
+			$('<button onclick=myFunform(this)>X</button>').appendTo(check)
+			//stor[head_select-1].sub[subhead_select-1].form.push({'input':itype_select, 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
 		}
 		else if (itype_select == "select"){
-			var selec=$('<p>')
+			var selec=$('<p data-type="'+itype_select+'" data-placeholder="'+pholder_value+'" id-holder="'+inputid_value+'" name-holder="'+name_value+'" op-holder="'+radioo_select+'" lab-holder="'+label_value+'">')
 			var select_tag=$('<select>').appendTo(selec)
 			var op_select_len =radioo_select.length;
 			for(i = 0; i < op_select_len; i++){					
@@ -139,21 +148,21 @@ $(document).ready(function(){
 				selec.appendTo('main section:nth-child('+head_select+' ) div:nth-child(' +cc+ ') ')
 			}
 			$('<button onclick=myFunform(this)>X</button>').appendTo(selec)
-			stor[head_select-1].sub[subhead_select-1].form.push({'input':'select', 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
+			//stor[head_select-1].sub[subhead_select-1].form.push({'input':itype_select, 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
 		}			
 		else if (itype_select == "textarea"){
-			var text_area=$('<p>')
+			var text_area=$('<p data-type="'+itype_select+'" data-placeholder="'+pholder_value+'" id-holder="'+inputid_value+'" name-holder="'+name_value+'" op-holder="'+radioo_select+'" lab-holder="'+label_value+'">')
 			$('<label>'+label_value+'</label><textarea id="'+inputid_value+'" name="'+name_value+'"  placeholder="'+pholder_value+'"></textarea>').appendTo(text_area)
 			text_area.appendTo('main section:nth-child('+head_select+' ) div:nth-child(' +cc+ ') ')
 			$('<button onclick=myFunform(this)>X</button>').appendTo(text_area)
-			stor[head_select-1].sub[subhead_select-1].form.push({'input':'textarea', 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
+			//stor[head_select-1].sub[subhead_select-1].form.push({'input':itype_select, 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value, 'option':radioo_select});
 		}
 		else {
-			var others_it=$('<p>')
-			$('<label>'+label_value+'</label><input type="'+itype_select+'" id="'+inputid_value+'" name="'+name_value+'"  placeholder="'+pholder_value+'"></textarea>').appendTo(others_it)
+			var others_it=$('<p data-type="'+itype_select+'" data-placeholder="'+pholder_value+'" id-holder="'+inputid_value+'" name-holder="'+name_value+'" op-holder="'+radioo_select+'" lab-holder="'+label_value+'">')
+			$('<label>'+label_value+'</label><input type="'+itype_select+'" id="'+inputid_value+'" name="'+name_value+'"  placeholder="'+pholder_value+'">').appendTo(others_it)
 			others_it.appendTo('main section:nth-child('+head_select+' ) div:nth-child(' +cc+ ') ')
 			$('<button onclick=myFunform(this)>X</button>').appendTo(others_it)
-			stor[head_select-1].sub[subhead_select-1].form.push({'input':itype_select, 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value});
+			//stor[head_select-1].sub[subhead_select-1].form.push({'input':itype_select, 'label':label_value, 'id':inputid_value, 'name':name_value, 'value':value_value, 'placeholder':pholder_value});
 			}
 		if ($('.check_class').is(":checked")){
 			var che_ck = itype_select
@@ -189,9 +198,10 @@ $(document).ready(function(){
 		// 	$('main section:nth-child('+head_select+' ) div:nth-child(' +cc+ ') p:last-child '+disb).attr('disabled','disabled');
 		// }
 		
-	localStorage.setItem('stor',JSON.stringify(stor));
+		//localStorage.setItem('stor',JSON.stringify(stor));
 	
-	$(".formhead_subhead").trigger('reset');
+		//$(".formhead_subhead").trigger('reset');
+		strgeform();
 	});     
 });
 
@@ -200,7 +210,7 @@ function myFunction(t) {
 	$(t).parent().parent().remove();
 	$(".head_dropdown1 option").remove();
 	$(".formhead_list option").remove(); 
-	$( "main h1" ).each(function(key) {
+	$( "main h1" ).each(function(key) {    //adding so that heading do not disappear from second and third form.
 		key=key+1
 		var head = $(this)
 		.clone()    //clone the element
@@ -209,16 +219,47 @@ function myFunction(t) {
 		.end()  //again go back to selected element
 		.text();		
 	  $(".form-group select").append('<option value="'+key+'">'+ head +'</option>');
-	  $(".form-group1 select").append('<option value="'+key+'">'+ head +'</option>');	  
+	  $(".form-group1 select").append('<option value="'+key+'">'+ head +'</option>');	  	
 	});
+	strgeform()
 }
 
 function myFunctionn(u) {
 	$(u).parent().parent().remove();
 	$(".formsubhead_list option").remove();
+	strgeform()
 }
 
 function myFunform(v) {
   $(v).parent().remove();
+  strgeform()
+  // $(localStorage.removeItem(v));
+}
+
+function strgeform() {
+	var stor = [];
+	$("main section h1").each(function(hkey,hvalue){
+		hkey=hkey+1
+		var head = $(this).text().replace("X","");
+		stor.push({'title':head,'sub':[]});		
+		$("main section:nth-child(" +hkey+ ") div h2").each(function(shkey,shvalue){
+			shkey=shkey+2
+			var subhead = $(this).text().replace("X","");
+			console.log(shkey)			
+			stor[hkey-1].sub.push({'title':subhead,'formm':[] });	
+			$("main section:nth-child("+hkey+") div:nth-child("+shkey+") p").each(function(fkey,fvalue){
+				var dtype= $(this).attr('data-type')
+				var lab_holder= $(this).attr('lab-holder')
+				var p_holder= $(this).attr('data-placeholder')
+				var id_holder= $(this).attr('id-holder')
+				var name_holder= $(this).attr('name-holder')
+				var op_holder= $(this).attr('op-holder')
+				var value_holder= $(this).attr('value-holder')
+				
+				stor[hkey-1].sub[shkey-2].formm.push({'input':dtype, 'label':lab_holder, 'id':id_holder, 'name':name_holder, 'value':value_holder, 'placeholder':p_holder, 'option':op_holder});
+			});
+		});
+	});
+	localStorage.setItem('stor',JSON.stringify(stor));
 }
 
